d7343fd145feb53f3119bcbcf2805a6248c9bd4a
Pavle <paavlee@yahoo.com>
2015-04-13 10:09:15 +0200

    Default css

---
+++ models/defaultCSSData.js
@@ -0,0 +1,10 @@
+/** Author : Pavle Paunovic */
+
+
+var cssMixin = Backbone.Model.extend({
+    defaults : {
+        backgroundColor : "#3F4041",
+        color: "#E9E6E4",
+        padding: "15px 15px 15px 15px"
+    }
+});
